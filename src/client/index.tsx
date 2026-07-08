c710784488ef86e63f6f8e0023ea276872524dc7
mms10mms <mikemms497@gmail.com>
2026-07-08 12:40:05 -0400

    Update index.tsx

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -90,7 +90,7 @@ function App() {
 
 	return (
 		<div className="App">
-			<h1>we see the unseen</h1>
+			<h1>security is an illusion</h1>
 			{counter !== 0 ? (
 				<p>
 					<b>{counter}</b> {counter === 1 ? "person" : "people"} connected.
